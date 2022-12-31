e0033878f68d0fc2f2a09cf7149fb0e767ddcb63
areain10 <107684386+areain10@users.noreply.github.com>
2022-12-31 19:16:44 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let intro = document.querySelector('.intro');
 let name = document.querySelector('.name-header');
 let nameSpan = document.querySelectorAll('.name');
-
+let main = document.querySelector('.main');
 window.addEventListener('DOMContentLoaded',()=>{
         setTimeout(()=>{
                 for(let i=0;i<3;i++){
@@ -27,5 +27,39 @@ window.addEventListener('DOMContentLoaded',()=>{
 	},5000)
         setTimeout(()=>{
                 intro.style.top= '-100vh';
-        },9000);
+        },8000);
+	setTimeout(()=>{
+		main.classList.add('active');
+	},10000);
+	setTimeout(()=>{
+		cursor.style.display ="block";
+	},13000);
+	
 });
+
+let cursor = document.querySelector(".cursor");
+document.addEventListener('mousemove',(e)=>{
+	let x = e.clientX;
+	let y = e.pageY;
+	cursor.style.left = x+"px";
+	cursor.style.top = y+"px";
+});
+document.addEventListener('wheel',(e)=>{
+	let x = e.clientX;
+	let y = e.pageY;
+	cursor.style.left = x+"px";
+	cursor.style.top = y+"px";
+});
+
+let dd = document.querySelectorAll(".dd");
+
+dd.forEach((d,i)=>{
+
+	d.addEventListener('mouseover',()=>{
+		cursor.classList.add('hov');
+	});
+	d.addEventListener('mouseout',()=>{
+		cursor.classList.remove('hov');
+	});
+});
+
